b12ba47ebb1f89d591314f7787d627e810936e85
n0it <52136313+n0it@users.noreply.github.com>
2020-03-26 18:52:58 +0100

    Add files via upload
    lol

--- IceField/src/logic/items/BackPack.java
+++ IceField/src/logic/items/BackPack.java
@@ -7,12 +7,15 @@ public class BackPack {
 	private HashMap<PlayerActions, ArrayList<Items>> obtainedItems = new HashMap<>();
 
 	public Items hasItem(PlayerActions pa) {
+		System.out.println("BackPack.hasItem() meghívódott");
 	}
 	public boolean addItem(Items it, PlayerActions pa) {
+		System.out.println("BackPack.addItem() meghívódott");
 	}
 	public Food useFood() {
+		System.out.println("BackPack.useFood() meghívódott");
 	}
 	public int getEssentialItemNumber() {
+		System.out.println("BackPack.getEssentialItemNumber() meghívódott");
 	}
-	public int getNumber(PlayerActions pa){ } //CSAK TESZT
 }
--- IceField/src/logic/items/Divingsuit.java
+++ IceField/src/logic/items/Divingsuit.java
@@ -5,8 +5,10 @@ import logic.characters.Character;
 public class Divingsuit implements Items {
 
 	public void use(Character actualch) {
-
+		System.out.println("Divingsuit.use() meghívódott");
 	}
-	public boolean equip(Character ch) { }
+	public boolean equip(Character ch) {
+		System.out.println("Divingsuit.equip() meghívódott");
+	}
 
 }
