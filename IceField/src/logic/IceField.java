be66d8660ed562c2b959ed46e58e754e7e9e1d30
ngellert <gellert.nagy99@gmail.com>
2020-03-26 18:57:05 +0100

    dsfds

--- IceField/src/logic/IceField.java
+++ IceField/src/logic/IceField.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package logic;
 
 import logic.characters.Character;
@@ -71,3 +72,67 @@ public class IceField {
 
 	}
 }
+=======
+package logic;
+
+import logic.characters.Character;
+import logic.icecells.IceCell;
+import logic.icecells.StableIceCell;
+import logic.icecells.UnstableIceCell;
+import logic.icecells.WaterCell;
+import logic.items.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class IceField {
+	private static int maxPlayer;
+	private static int fieldLengths;
+	private int currentPlayer = 0;
+	public boolean gameWon = false;
+	public boolean gameLost = false;
+	private List<List<IceCell>> field = new ArrayList<>();
+	private ArrayList<Character> characters;
+	private WinChecker wc = new WinChecker();
+    private int chosenToSave = -1;
+
+	public IceField(){ }
+
+	private void snowStorm() {
+		System.out.println("IceField.snowStorm() meghívódott");
+	}
+
+	public void addIceCell(IceCell ic, IceCell removed) {
+		System.out.println("IceField.addIceCell() meghívódott");
+	}
+	private void gameLost() {
+		System.out.println("IceField.gameLost() meghívódott");
+	}
+	private void gameWon() {
+		System.out.println("IceField.gameLost() meghívódott");
+	}
+
+	public void nextPlayer() {
+		System.out.println("IceField.nextPlayer() meghívódott");
+	}
+	private void useEssentialItems() {
+		System.out.println("IceField.useEssentialItems() meghívódott");
+	}
+	public void setPlayerWay(Way w){
+		System.out.println("IceField.setPlayerWay() meghívódott");
+	}
+	public void usePlayerItem(PlayerActions pa) {
+		System.out.println("IceField.usePlayerItem() meghívódott");
+	}
+	public void useAbility() {
+		System.out.println("IceField.useAbility() meghívódott");
+	}
+	public void movePlayer(Way w) {
+		System.out.println("IceField.movePlayer() meghívódott");
+	}
+	public void mineActualCell() {
+		System.out.println("IceField.mineActualCell() meghívódott");
+	}
+}
+>>>>>>> b12ba47ebb1f89d591314f7787d627e810936e85
--- IceField/src/logic/items/BackPack.java
+++ IceField/src/logic/items/BackPack.java
@@ -7,12 +7,19 @@ public class BackPack {
 	private HashMap<PlayerActions, ArrayList<Items>> obtainedItems = new HashMap<>();
 
 	public Items hasItem(PlayerActions pa) {
+		System.out.println("BackPack.hasItem() meghívódott");
 	}
 	public boolean addItem(Items it, PlayerActions pa) {
+		System.out.println("BackPack.addItem() meghívódott");
 	}
 	public Food useFood() {
+		System.out.println("BackPack.useFood() meghívódott");
 	}
 	public int getEssentialItemNumber() {
+		System.out.println("BackPack.getEssentialItemNumber() meghívódott");
 	}
+<<<<<<< HEAD
 	public int getNumber(PlayerActions pa){ }
+=======
+>>>>>>> b12ba47ebb1f89d591314f7787d627e810936e85
 }
