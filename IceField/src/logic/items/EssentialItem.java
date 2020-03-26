b1adba889fe35456ab994c9aa47e654981f399d9
n0it <52136313+n0it@users.noreply.github.com>
2020-03-26 18:24:06 +0100

    Add files via upload
    -items

--- IceField/src/logic/items/EssentialItem.java
+++ IceField/src/logic/items/EssentialItem.java
@@ -8,13 +8,11 @@ public class EssentialItem implements Items {
 	private WinChecker wc;
 
 	public EssentialItem(int id, WinChecker winch){
-		wc = winch;
-		ID = id;
+
 	}
 
 	public void use(Character actualch) {
-		int sum = actualch.getBackPack().getEssentialItemNumber();
-		wc.addEssentials(sum);
+
 	}
-	public boolean equip(Character ch) { return ch.putItemtoBackPack(this, PlayerActions.assemblingEssentials);}
+	public boolean equip(Character ch) { }
 }
--- IceField/src/logic/items/Food.java
+++ IceField/src/logic/items/Food.java
@@ -5,7 +5,6 @@ import logic.characters.Character;
 public class Food implements Items {
 
 	public void use(Character actualch) {
-		actualch.gainOneHeat();
 	}
-	public boolean equip(Character ch) { return ch.putItemtoBackPack(this, PlayerActions.eating);}
+	public boolean equip(Character ch) { }
 }
